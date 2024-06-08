a58dc85dc2954514e4a9e027066f7101588946f6
Yonvay <yvargasgu@unal.edu.co>
2024-06-08 07:52:50 -0500

    Creacion de carpeta almacenadora

---
+++ Proyecto/Entrega final, Oscar Riveros, Yovany Vargas/Aplicación/Functions.py
@@ -0,0 +1,18 @@
+import cv2
+from IPython.display import Image
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+import os
+import time
+from scipy import ndimage as ndi
+from scipy import signal
+from skimage import graph, color, filters, morphology, segmentation, measure, feature
+
+# Funciones para la adecuación de las imágenes
+def loadImg(img):
+    
+    return img
+
+print('Optimo')
